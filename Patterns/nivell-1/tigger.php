47826b0fe39ce54935b14d39c620e5dd5027a585
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-12-13 12:02:36 +0100

    singleton sketch

--- Patterns/nivell-1/tigger.php
+++ Patterns/nivell-1/tigger.php
@@ -1,11 +1,39 @@
-class Tigger {
+/*
+Imprimeix en pantalla múltiples vegades el rugit de Tigger.
+Afegeix un mètode getCounter () que retorni el nombre de vegades que Tigger ha realitzat rugits.
+*/
 
-       private function __construct() {
-               echo "Building character..." . PHP_EOL;
-       }
+<?php
+class Tigger{
+        private static $instances = [];
 
-       public function roar() {
-               echo "Grrr!" . PHP_EOL;
-       }
+        //El constructor ha de ser private per evitar que es creïn noves instàncies de la classe
+        //Només la pròpia classe pot crear una sola instància
+        private function __construct()
+        {
+                echo "Building character..." . PHP_EOL;
+        }
 
+        public function roar()
+        {
+                echo "Grrr!" . PHP_EOL;
+        }
+        //No es pot clonar o serialitzar, per això s'edita la funció clone() i wakeup() amb protected o private
+        protected function __clone(){}
+        protected function __wakeup(){}
+
+        //getInstance comprova que ja existeix una instància de la classe 
+        //creada pel constructor, i fa un return de la instància
+        public static function getInstance(): Tigger
+        {
+                $class = static::class;
+                if (!isset(self::$instances[$class])) {
+                        self::$instances[$class] = new static();
+                }
+
+                return self::$instances[$class];
+        }
 }
+
+
+?>
